ec5816700ed1c5710d537ae849f6cc3c9e586228
Tahfizul Hasan Zihan <tahfijulhassanjehan@gmail.com>
2024-06-29 18:36:52 +0600

    Revert "dummy changes"
    This reverts commit 5de75931f52cd4273a4ccbd2a04d5a139c203782.

--- Final_OOP_MPS/src/main/java/com/group47/main_pkg/final_oop_mps/MainApplicationClass.java
+++ Final_OOP_MPS/src/main/java/com/group47/main_pkg/final_oop_mps/MainApplicationClass.java
@@ -11,8 +11,8 @@ public class MainApplicationClass extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplicationClass.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello world!");
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
