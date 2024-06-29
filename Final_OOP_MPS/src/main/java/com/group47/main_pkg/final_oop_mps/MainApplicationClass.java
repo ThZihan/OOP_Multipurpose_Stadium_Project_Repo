5de75931f52cd4273a4ccbd2a04d5a139c203782
Tahfizul Hasan Zihan <tahfijulhassanjehan@gmail.com>
2024-06-29 18:35:24 +0600

    dummy changes

--- Final_OOP_MPS/src/main/java/com/group47/main_pkg/final_oop_mps/MainApplicationClass.java
+++ Final_OOP_MPS/src/main/java/com/group47/main_pkg/final_oop_mps/MainApplicationClass.java
@@ -11,8 +11,8 @@ public class MainApplicationClass extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplicationClass.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello world!");
         stage.setScene(scene);
         stage.show();
     }
